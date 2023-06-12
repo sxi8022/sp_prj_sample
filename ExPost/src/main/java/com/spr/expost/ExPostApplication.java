140615954dd654283917a9dda1c6c575b3315787
sxi8022 <sxi8022@gmail.com>
2023-06-12 20:40:11 +0900

    초반셋팅

---
+++ ExPost/src/main/java/com/spr/expost/ExPostApplication.java
@@ -0,0 +1,13 @@
+package com.spr.expost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExPostApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExPostApplication.class, args);
+    }
+
+}
---
+++ ExPost/src/test/java/com/spr/expost/ExPostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spr.expost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExPostApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
